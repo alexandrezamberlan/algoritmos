592f40fa94e319d09f7b9bc2829a99f19709f123
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2025-10-20 20:39:33 -0300

    ponteiro5

---
+++ 07-Ponteiros/ponteiro5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#define TAM 5
+using namespace std;
+
+int main() {
+    string vetor[TAM];
+    //construa um método que preencha o vetor com nomes lidos do teclado
+
+    //construa um método que exiba os nomes do vetor
+
+    //construa um método que receba o vetor e um nome a ser buscado, 'retorne' a posição do nome no vetor. Caso o nome não exista, 'retorne' -1. Não é permitido usar return
+}
